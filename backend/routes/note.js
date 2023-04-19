64e10d7c310f23a19841455f84f2d9384e658314
Gokhlesh Kumar <gokhleshns@gmail.com>
2023-04-19 14:21:32 +0530

    conc

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -49,4 +49,63 @@ router.post('/addnotes', fetchuser, [
 
 })
 
+
+
+
+// Route 3: Update the notes of user: PUT "/api/notes/update" Login required
+router.put('/update/:id', fetchuser, async(req, res) => {
+    
+    const {title, description, tag} = req.body;
+
+    try {
+        // Creating new Note to upadte
+        const newNote = {};
+        if(title) {newNote.title = title};
+        if(description) {newNote.description = description};
+        if(tag) {newNote.tag = tag};
+
+        // Find the node to be updated and update it.
+        let note = await Note.findById(req.params.id);
+        if(!note) { return res.status(401).send({error : 'Wrong user ID'})}
+
+        // Allow updation only if user own this note
+        if(note.user.toString() !== req.user.id) { return res.status(401).send({error : 'Not Allowed'});}
+
+        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {$new : true});
+        res.json(note);
+
+    
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+
+})
+
+
+
+
+// Route 4: Delete the existing notes of user: DELETE "/api/notes/delete" Login required
+router.delete('/delete/:id', fetchuser, async(req, res) => {
+    
+    try {
+
+        // Find the node to be deleted and delete it.
+        let note = await Note.findById(req.params.id);
+        if(!note) { return res.status(401).send({error : 'Wrong user ID'})}
+
+        // Allow deletion only if user own this note
+        if(note.user.toString() !== req.user.id) { return res.status(401).send({error : 'Not Allowed'});}
+
+        note = await Note.findByIdAndDelete(req.params.id);
+        res.json({"Success" : "Note has been deleted" , note : note});
+
+    
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+
+})
+
 module.exports = router
--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes, Route
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import About from './Components/About';
+import Home from './Components/home';
+
 
 function App() {
   return (
     <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/about' element={<About/>}/>
+      </Routes>
+    </Router>
     </>
   );
 }
